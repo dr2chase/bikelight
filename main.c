4982d46725ccdbee976a7fc897d872a73efed668
David Chase <dr2chase@mac.com>
2015-04-20 20:13:43 -0400

    Added license (Creative Commons Attribution-ShareAlike 4.0 International License)

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 /*
- * Copyright (c) 2015, David R. Chase.  All rights reserved.
+ * Copyright (c) 2015, David R. Chase.
+ * This work is licensed under the Creative Commons Attribution 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/4.0/
+ * or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
  */
 
 #include <stdint.h>
